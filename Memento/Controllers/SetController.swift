77c29ba0c7dc89fd503c73d2c36359d5a6dd85c9
Jonah Kornberg <jonahkornberg@Jonahs-MacBook-Pro.local>
2018-10-23 21:25:29 -0400

    Complete card data structure, Home and Select Card set screen, checkmarks

--- Memento/Controllers/SetController.swift
+++ Memento/Controllers/SetController.swift
@@ -48,20 +48,6 @@ class SetController: UIViewController, UITableViewDelegate, UITableViewDataSourc
         print("selected \(indexPath)")
     }
     
-    
-    func randomStringWithLength (len : Int, notIn: [CardSet]) -> String {
-        
-        let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-        
-        let randomString : NSMutableString = NSMutableString(capacity: len)
-        for _ in 0...len{
-            let length = UInt32 (letters.length)
-            let rand = arc4random_uniform(length)
-            randomString.appendFormat("%C", letters.character(at: Int(rand)))
-        }
-        
-        return randomString as String
-    }
     /*
     // MARK: - Navigation
 
